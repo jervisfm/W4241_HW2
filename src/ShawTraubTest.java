e8ee5d75f74b0b6a6c775428f294070f03ad4096
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-02-17 18:45:11 -0500

    update the shaw traub test

--- src/ShawTraubTest.java
+++ src/ShawTraubTest.java
@@ -21,11 +21,13 @@ public class ShawTraubTest {
 		
 		double expected_d0 = 1 + 2*x + x*x;
 		double expected_d1 = 2*x + 2*x*x;
+		double expected_d2 = 1;
 		
 		ArrayList<Double> actual = ShawTraub.getDerivatives(p, x);
-		
+		System.out.println("Actual size ==" + actual.size());
 		assertEquals(expected_d0, actual.get(0), err);
 		assertEquals(expected_d1, actual.get(1), err);
+		assertEquals(expected_d2, actual.get(2), err);
 		
 	}
 
