49bab6f072ee183abe1f2e7e3a3bb154199f1489
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-02-17 18:56:06 -0500

    add polynomial reader

---
+++ src/PolyReader.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ * Reads a Polynomial From a text file
+ * @author Jervis
+ *
+ */
+public class PolyReader {
+
+	
+	public static Polynomial fromString(String s) {
+		/**
+		 * Format is: 
+		 * [Polynomial Order]
+		 * [Coefficeints separated by space]
+		 * [the x - value to evaluate]  
+		 */
+		
+		Scanner in = new Scanner(s);
+		
+		int polyOrder = in.nextInt(); 
+		ArrayList<Double> coeff = new ArrayList<Double>(polyOrder + 1);
+		
+		for (int i = 0; i <=polyOrder; ++i) {
+			double val = in.nextDouble();
+			coeff.add(val);
+		}
+		Polynomial p = new Polynomial(coeff);
+		return p;
+	}
+	
+	public static double getXValue(String s) {
+		s = s.trim();
+		Scanner in = new Scanner(s); 
+		in.nextLine(); 
+		in.nextLine(); 
+		return in.nextDouble();
+	}
+}
